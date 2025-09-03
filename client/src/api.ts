02e964bb299a85ab458091119175d36ee5778998
puru yadav <puruyadav1004@purus-MacBook-Pro.local>
2025-09-03 18:14:35 +0530

    update

--- client/src/api.ts
+++ client/src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
-// const API_BASE = import.meta.env.VITE_BACKEND_URL as string;
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = import.meta.env.VITE_BACKEND_URL as string;
+// const API_BASE = "http://127.0.0.1:8000";
 
 export async function getPrediction(stock_symbol: string, days_ahead: number) {
   try {
